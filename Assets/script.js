3841c79b0245abcbfba684f2ae0cc11bab8f95a7
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-01-26 10:35:53 -0500

    revised script to make planner display row and column with time,txtarea and save button

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    
 
-    var DateTime = luxon.DateTime.local().setZone("America/New_york")
+    var DateTime = luxon.DateTime.local().setZone("America/New_york");
+    var currentHour = DateTime.toFormat("HH");
 
     var currentDay = DateTime.toLocaleString({
         weekday: 'long',
@@ -10,32 +10,85 @@ $(document).ready(function () {
         year: 'numeric',
         hour: 'numeric',
         minute: 'numeric',
-        
+
     });
-  
+
     $("#currentDay").text(currentDay);
 
-    var hour = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "6:00 PM", ];
-   
-    for (var i=0; i < hour.length; i++) {
-        var time = $("<div>",);
-        time.addClass("row row-cols-auto time-block");
-        time.attr("time", hour[i]);
-        time.text(hour[i]);
+    var hour = [
 
-        $(".container").append(time);
-        
-        var textArea = $("div");
-        textArea.addClass("col-sm-10 description textarea");
+        {
+            hourDisplay: "09:00 AM",
+            hour: 9
+        },
+        {
+            hourDisplay: "10:00 AM",
+            hour: 10
+        },
+        {
+            hourDisplay: "11:00 AM",
+            hour: 11
+        },
+        {
+            hourDisplay: "12:00 PM",
+            hour: 12
+        },
+        {
+            hourDisplay: "01:00 PM",
+            hour: 13
+        },
+        {
+            hourDisplay: "02:00 PM",
+            hour: 14
+        },
+        {
+            hourDisplay: "03:00 PM",
+            hour: 15
+        },
+        {
+            hourDisplay: "04:00 PM",
+            hour: 16
+        },
+        {
+            hourDisplay: "05:00 PM",
+            hour: 17
+        },
+    ];
+    //var hour = [09, 10, 11, 12, 13, 14, 15, 16, 17 ]
 
-        time.append(textArea);
+    $(".container").addClass("time-block");
 
-        var button =$("<button>");
-        button.addClass("saveBtn far fa-save");
-        time.append(button)
+     hour.forEach(function (hour) {
 
-    }
+        var row = $("<div>").addClass("row");
+        $(".container").append(row);
 
+        var time = $("<div>").addClass("col-1 hour");
+        time.text(hour.hourDisplay);
+        row.append(time);
+
+        var textInput = $("<input>").addClass("textArea col-10 description");
+        row.append(textInput);
+
+        var saveBtn = $("<button>").addClass("saveBtn col-1");
+        row.append(saveBtn);
+
+        var btnImg = $("<i>").addClass("far fa-save fa-lg");
+        saveBtn.append(btnImg);
+
+    });
+
+
+
+        if (hour.hour === currentHour) {
+            textArea.addClass("present");
+        } else if (hour.hour < currentHour) {
+            textArea.addClass("past");
+        } else {
+            textArea.addClass("future");
+        }
+
+    
 
 
 
